22b699258101a5c1f05ebf1b77cc80af823c57df
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2023-02-15 22:31:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import string
+
+alfabets = list(string.ascii_lowercase)
+
+burti = (("a", "ā"), 
+         ("c", "č"), 
+         ("e", "ē"), 
+         ("g", "ģ"), 
+         ("i", "ī"), 
+         ("k", "ķ"), 
+         ("l", "ļ"),
+         ("n", "ņ"),
+         ("s", "š"),
+         ("u", "ū"),
+         ("z", "ž"),
+        )
+
+for vecaisBurts, jaunaisBurts in burti:
+  alfabets.insert(alfabets.index(vecaisBurts) + 1, jaunaisBurts)
+  
+liekieBurti = ["q", "w", "x", "y"]
+for burts in liekieBurti:
+  alfabets.remove(burts)
+
+print(alfabets)
+
+teksts = "zivs peld upē"
+jaunaisTeksts = str()
+k = 3
+
+def switchIndex(idx):
+  for _ in range(k):
+    if k > 0:
+      idx += 1
+      if idx >= len(alfabets): 
+        idx = 0
+    elif k < 0:
+      idx -= 1
+      if idx < 0: 
+        idx = len(alfabets) - 1
+    
+  return idx
+
+for simbols in teksts:
+  if simbols in alfabets:
+    indekss = alfabets.index(simbols)
+    print(indekss)
+    indekss = switchIndex(indekss)
+    print(indekss)
+    
+    jaunaisTeksts += alfabets[indekss]
+  else: jaunaisTeksts += simbols
+
+print(jaunaisTeksts)
